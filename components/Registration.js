633d139044d2a4a0fc7dd90fa69aaec11de8eba8
Tony Eneh <aeneh90@gmail.com>
2020-11-14 07:05:12 +0100

    Feat: Create components

---
+++ components/Registration.js
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export default function Registration() {
+  return (
+    <View>
+      <Text>Registration</Text>
+    </View>
+  );
+}
---
+++ components/index.js
@@ -0,0 +1,9 @@
+import Header from './Header';
+import About from './About';
+import Login from './Login';
+import Logout from './Logout';
+import Profile from './Profile';
+import Registration from './Registration';
+import Settings from './Settings';
+
+export {Header, About, Login, Logout, Profile, Registration, Settings};
